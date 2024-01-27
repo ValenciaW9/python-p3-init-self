db40a551d8999bb9b7fa853e5da7bc8e639e7089
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-01-27 16:44:32 -0500

    python-p3-init-self/
    python-p3-init-self/

---
+++ python-p3-init-self-main/lib/dog.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+class Dog:
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
