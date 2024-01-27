55b6fb3ad04729e51e83f2ca0246b29ed2ece432
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-01-27 16:49:23 -0500

    python-p3-init-self/
    python-p3-init-self/

--- testing/person_test.py
+++ testing/person_test.py
@@ -2,13 +2,18 @@
 
 from person import Person
 
-
 class TestPerson:
+    '''Person in person.py'''
 
     def test_is_class(self):
+        '''is a class with the name "Person"'''
         guido = Person("Guido")
-        assert isinstance(guido, Person)
+        assert(type(guido) == Person)
+
+class TestInit:
+    '''Person.__init__ in person.py'''
 
     def test_saves_self_name(self):
+        '''takes a name as an argument and saves it to self.name'''
         guido = Person("Guido")
-        assert guido.name == "Guido"
+        assert(guido.name == "Guido")
